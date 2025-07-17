0ac5760f5caa6fc1d5d861a6e72eda4db2853e1f
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-17 09:54:43 -0600

    Add files via upload

---
+++ estaciones_mx0_spmx5_1_2_copia.js
@@ -0,0 +1,11 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_spmx5_1_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "MexiquenseParks, 54900, De las Torres 191, Sin Nombre, 54910 Fuentes del Valle, Méx.", "description": "MX0", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.1476988, 19.6397536, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Sauce 25, Tlatel Xochitenco, 56366 Chimalhuacán, Méx.", "description": "SPMX5", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -98.9725021, 19.4471262, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "FWRR+24J, Cdad. Nezahualcóyotl, Méx.", "description": "SPMX1", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.0597027, 19.490075, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Tlalnepantla 26, Sta Maria Tulpetlac, 55500 Ecatepec de Morelos, Méx.", "description": "SPMX2", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.0531977, 19.5609582, 0.0 ] } }
+]
+}
